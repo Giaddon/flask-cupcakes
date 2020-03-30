7cfcc29e3a900cb6e7ba8b30cf18c7687c3c3af6
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-03-30 12:33:14 -0700

    Can view and create cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,77 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, jsonify, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "Shhhhh"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+debug = DebugToolbarExtension(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    "Returns JSON object containing all cupcakes"
+    cupcake_list = Cupcake.query.all()
+
+    serialized_list = [c.serialize() for c in cupcake_list]
+
+
+    return jsonify(cupcakes=serialized_list)
+
+
+@app.route("/api/cupcakes/<int:id>")
+def inspect_cupcake(id):
+    "Returns JSON object containing data about specified cupcake"
+
+    cupcake = Cupcake.query.get_or_404(id)
+    serialized_cupcake = cupcake.serialize()
+
+    return jsonify(cupcake=serialized_cupcake)
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+
+    flavor = ""
+    size = ""
+    rating = 0
+    image = None
+
+
+    try:
+        flavor = request.json["flavor"]
+    except:
+        return ("Error with data submission", 400)
+
+    try:
+        size = request.json["size"]
+    except:
+        return ("Error with data submission", 400)
+
+    try:
+        rating = request.json["rating"]
+    except:
+        return ("Error with data submission", 400)
+
+    image = request.json.get("image", None)
+
+    new_cupcake = Cupcake(flavor=flavor, 
+                          size=size,
+                          rating=rating,
+                          image=image) 
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return ( jsonify(cupcake=new_cupcake.serialize()), 201 )
--- models.py
+++ models.py
@@ -1,1 +1,40 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake Class"""
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String,
+                       nullable=False)
+    size = db.Column(db.String,
+                     nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image  = db.Column(db.String, 
+                       default="https://tinyurl.com/demo-cupcake",
+                       nullable=False)
+
+    def serialize(self):
+        """Returns dictionary with my attributes
+        mapped to keys and values."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        } 
